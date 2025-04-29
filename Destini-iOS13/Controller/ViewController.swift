213fb5740cb00ac93beb786c29ba0840d806e54f
Isaías Chávez Martínez <isaiaschavez.co@outlook.com>
2025-04-29 00:46:19 -0600

    final

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -3,22 +3,34 @@
 //  Destini-iOS13
 //
 //  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Copyright © 2019 The App Brewery.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    @IBAction func handleClick(_ sender: UIButton) {
+       
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
+  
+    
+    func updateUI() {
+        let story = storyBrain.getCurrentStory()
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
 }
-
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,9 +1,10 @@
-//
-//  Sotry.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
+// Story.swift
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
